3ef82236dfc6aee3c4a6822ff24f2824385fcbfe
Matt McPartlan <mmcpartlan98@gmail.com>
2018-09-19 12:36:40 -0700

    Javadocs

--- Homework/Homework01/HighArrayApp.java
+++ Homework/Homework01/HighArrayApp.java
@@ -1,32 +1,50 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*  File name     :  HighArrayApp.java
+*  Purpose       :  This class tests the HighArray class.
+*  Author        :  M. Abell & M. McPartlan
+*  Date          :  2018-09-19
+*  Description   :  This class tests the indexing for advanced array operations.
+*                   Functions to test:
+*                   public HighARray( int max );                  // Constructor for array of length max
+*                   public boolean find(long searchKey);          // Checks for elements in the array with value searchkey
+*                   public void insert(long value);               // Inserts value into array
+*                   public boolean delete(long value);            // Removes value from array
+*                   public void display();                        // Displays the array
+*                   public long getMax();                         // Returns maximum value in array
+*                   public void noDups();                         // Removes duplicate entries
+*
+*  Exceptions    :  None
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 class HighArrayApp{
-	public static void main(String[] args){
-		int maxSize = 100; // array size
-		HighArray arr; // reference to array
-		arr = new HighArray(maxSize); // create the array
-		arr.insert(77); // insert 10 items
-		arr.insert(99);
-		arr.insert(44);
-		arr.insert(55);
-		arr.insert(22);
-		arr.insert(88);
-		arr.insert(11);
-		arr.insert(00);
-		arr.insert(66);
-		arr.insert(33);
-		arr.display(); // display items
-		int searchKey = 35; // search for item
-		if( arr.find(searchKey) )
-			System.out.println("Found " + searchKey);
-		else
-			System.out.println("Can not find " + searchKey);
-		arr.delete(00); // delete 3 items
-		arr.delete(55);
-		arr.delete(99);
-		arr.insert(77);
-		arr.insert(22);
-		arr.display(); // display items again
-		System.out.println(arr.getMax());
-		arr.noDups();
-		arr.display();
-	} // end main()
+  public static void main(String[] args){
+    int maxSize = 100; // array size
+    HighArray arr; // reference to array
+    arr = new HighArray(maxSize); // create the array
+    arr.insert(77); // insert 10 items
+    arr.insert(99);
+    arr.insert(44);
+    arr.insert(55);
+    arr.insert(22);
+    arr.insert(88);
+    arr.insert(11);
+    arr.insert(00);
+    arr.insert(66);
+    arr.insert(33);
+    arr.display(); // display items
+    int searchKey = 35; // search for item
+    if( arr.find(searchKey) )
+    System.out.println("Found " + searchKey);
+    else
+    System.out.println("Can not find " + searchKey);
+    arr.delete(00); // delete 3 items
+    arr.delete(55);
+    arr.delete(99);
+    arr.insert(77);
+    arr.insert(22);
+    arr.display(); // display items again
+    System.out.println(arr.getMax());
+    arr.noDups();
+    arr.display();
+  } // end main()
 } // end class HighArrayApp
