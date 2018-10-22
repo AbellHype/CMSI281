44e4a26beb5b903617336b34b3f1a7e7127cc412
Matt McPartlan <mmcpartlan98@gmail.com>
2018-10-22 13:51:41 -0700

    Added documentation

--- Homework/Homework02/CircularList.java
+++ Homework/Homework02/CircularList.java
@@ -1,6 +1,6 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 *  File name     :  CircularList.java
-*  Purpose       :  This class automates indexing for advanced array operations.
+*  Purpose       :  This class defined a circular list.
 *  Author        :  M. McPartlan & M. Abell
 *  Date          :  2018-09-19
 *  Description   :
@@ -13,6 +13,12 @@ class CircularList {
   public ListItem current;
   private int listSize;
 
+  //-----------------------------------------------------------
+  /**
+  * Constructor for CircularList class
+  * @param  initialValues a string array of all elements to be created in the circular list
+  * @return void
+  */
   public CircularList(String[] initialValues) {
     this.listSize = initialValues.length;
     this.current = new ListItem();
@@ -41,6 +47,12 @@ class CircularList {
     }
   }
 
+  //-----------------------------------------------------------
+  /**
+  * Prints the circular list. Does not affect the "current" node selected.
+  * @param  None
+  * @return void
+  */
   public void printList() {
     System.out.print("[");
     for (int i = 0; i < this.listSize; i++) {
@@ -53,14 +65,32 @@ class CircularList {
     }
   }
 
+  //-----------------------------------------------------------
+  /**
+  * Returns the size of the circular list
+  * @param  None
+  * @return void
+  */
   public int getSize(){
     return this.listSize;
   }
 
+  //-----------------------------------------------------------
+  /**
+  * Iterate the circular list. Selects the next element in the list.
+  * @param  None
+  * @return void
+  */
   public void step() {
     this.current = this.current.nextItem;
   }
 
+  //-----------------------------------------------------------
+  /**
+  * Selects node with desired string as "current"
+  * @param  searchArg a string value to search for in the array.
+  * @return Boolean result of list search
+  */
   public boolean find(String searchArg) {
     for (int i = 0; i < this.listSize; i++) {
       if (this.current.value == searchArg) {
@@ -72,7 +102,12 @@ class CircularList {
     return false;
   }
 
-  // Inserts item ahead of current
+  //-----------------------------------------------------------
+  /**
+  * Inserts new ListItem ahead of "current". Does NOT affect current.
+  * @param  newArg the value to be inserted into the list
+  * @return void
+  */
   public void insert(String newArg) {
     ListItem insertItem = new ListItem();
     insertItem.value = newArg;
@@ -80,7 +115,13 @@ class CircularList {
     this.current.nextItem = insertItem;
     this.listSize += 1;
   }
-  // Deletes item ahead of current, steps over deleted value
+
+  //-----------------------------------------------------------
+  /**
+  * Deletes node ahead of "current". "Current" IS AFFECTED and becomes the node after the deleted node.
+  * @param  None
+  * @return String value of deleted node.
+  */
   public String delete() {
     ListItem tempCurrent = this.current;
 
@@ -92,6 +133,12 @@ class CircularList {
     return retValue;
   }
 
+  //-----------------------------------------------------------
+  /**
+  * A class defining ListItem, the node element used in CircularList. No functions other than constructor.
+  * @param  None
+  * @return void
+  */
   class ListItem {
     public ListItem nextItem;
     public String value;
@@ -101,6 +148,12 @@ class CircularList {
     }
   }
 
+  //-----------------------------------------------------------
+  /**
+  * Test cases for the CircularList and ListItem classes.
+  * @param  None
+  * @return void
+  */
   public static void main(String[] args) {
     String[] testArray = {"Hello!", "This", "is", "a", "circular", "list", "!"};
     CircularList test = new CircularList(testArray);
--- Homework/Homework02/Josephus.java
+++ Homework/Homework02/Josephus.java
@@ -11,6 +11,11 @@ import java.util.Arrays;
 
 class Josephus {
 
+  //-----------------------------------------------------------
+  /**
+  * @param  jArray a string array input to generate the circular list. Generated in main
+  * @return the number that must be counted by to save the array index labeled "Josephus"
+  */
   public static int josephus(String[] jArray) {
     for (int jump = 2; jump < jArray.length; jump++) {
       CircularList josephus = new CircularList(jArray);
@@ -38,18 +43,30 @@ class Josephus {
     return -1;
   }
 
+  //-----------------------------------------------------------
+  /**
+  * Generate the "Josephus" array and run the josephus simulation
+  * @param  None
+  */
   public static void main(String[] args) {
-    int jArrayL = Integer.parseInt(args[0]);
-    int jPosition = Integer.parseInt(args[1]);
-
-    String[] jArrayG = new String[jArrayL];
-    for (int iJ = 0; iJ < jArrayL; iJ++) {
-      jArrayG[iJ] = String.valueOf(iJ + 1);
-      if (iJ == jPosition - 1) {
-        jArrayG[iJ] = "Josephus";
+    try {
+      int jArrayL = Integer.parseInt(args[0]);
+      int jPosition = Integer.parseInt(args[1]);
+      String[] jArrayG = new String[jArrayL];
+      for (int iJ = 0; iJ < jArrayL; iJ++) {
+        jArrayG[iJ] = String.valueOf(iJ + 1);
+        if (iJ == jPosition - 1) {
+          jArrayG[iJ] = "Josephus";
+        }
       }
+
+      System.out.println("Count by: " + josephus(jArrayG));
+    }
+    catch(Exception e) {
+      System.out.println("Illegal argument(s). See usage below: \n\n");
+      System.out.println("Josephus INT(People) INT(LinePosition)\n\n");
     }
 
-    System.out.println("Count by: " + josephus(jArrayG));
+
   }
 }
