0a5018a24270f859a0302fb1a1d960853dbe97e0
AbellHype <35485344+AbellHype@users.noreply.github.com>
2018-10-22 13:17:04 -0700

    Added notes

--- Homework/Homework02/DeQueue.java
+++ Homework/Homework02/DeQueue.java
@@ -1,6 +1,7 @@
-// queue.java
-// demonstrates queue
-// to run this program: C>java QueueApp
+// DeQueue.java
+// This class creates a double ended queue
+// Written by Matthew Abell and Matthew McPartlab
+//2018-10-22
 ////////////////////////////////////////////////////////////////
 class DeQueue{
 	private int maxSize;
@@ -12,26 +13,26 @@ class DeQueue{
 	public DeQueue(int s){ // constructor
 		maxSize = s;
 		queArray = new long[maxSize];
-		front = 0;
-		rear = -1;
+		front = 0; //the front is the right side of the dequeue
+		rear = -1; //the rear is the left side of the dequeue
 		nItems = 0;
 		}
 //--------------------------------------------------------------
-	public void insertRight(long j){ // put item at rear of queue
+	public void insertRight(long j){ // put item right side of the dequeue
 		if(rear == maxSize-1) // deal with wraparound
 			rear = -1;
 		queArray[++rear] = j; // increment rear and insert
 		nItems++; // one more item
 	}
 //--------------------------------------------------------------
-	public void insertLeft(long j){ // put item at rear of queue
+	public void insertLeft(long j){ // put item left side of the dequeue
 		if(front == 0) // deal with wraparound
 			front = maxSize;
-		queArray[--front] = j; // increment rear and insert
+		queArray[--front] = j; // decrement front and insert
 		nItems++; // one more item
 	}
 //--------------------------------------------------------------
-	public long removeLeft(){ // take item from front of queue
+	public long removeLeft(){ // take item from left side of the dequeue
 		long temp = queArray[front++]; // get value and incr front
 		if(front == maxSize) // deal with wraparound
 			front = 0;
@@ -39,39 +40,39 @@ class DeQueue{
 		return temp;
 	}
 //--------------------------------------------------------------
-	public long removeRight(){ // take item from front of queue
-		long temp = queArray[rear--]; // get value and incr front
+	public long removeRight(){ // take item from the right side of the dequeue
+		long temp = queArray[rear--]; // get value and decr rear
 		if(rear == -1) // deal with wraparound
 			front = maxSize;
 		nItems--; // one less item
 		return temp;
 	}
 //--------------------------------------------------------------
-	public long peekLeft(){ // peek at front of queue
+	public long peekLeft(){ // peek at lesft most value of dequeue
 		return queArray[front];
 	}
 //--------------------------------------------------------------
-	public long peekRight(){ // peek at front of queue
+	public long peekRight(){ // peek at the right most value of the dequeue
 		return queArray[rear];
 	}
 //--------------------------------------------------------------
-	public boolean isEmpty(){ // true if queue is empty
+	public boolean isEmpty(){ // true if dequeue is empty
 		return (nItems==0);
 	}
 //--------------------------------------------------------------
-	public boolean isFull(){ // true if queue is full
+	public boolean isFull(){ // true if dequeue is full
 		return (nItems==maxSize);
 	}
 //--------------------------------------------------------------
-	public int size(){ // number of items in queue
+	public int size(){ // number of items in dequeue
 		return nItems;
 	}
 
-	public void display(){
+	public void display(){ //displays dequeue
         if (isEmpty()){
         	System.out.println(" ");
         }
-        else if(rear == -1){
+        else if(rear == -1){   //if insertRight has not been used
         	rear = 0;
         	int i = front;
 			while (i != rear){
