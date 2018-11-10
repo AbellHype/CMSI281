a53ac203754a19a28da573156b0c0ee782f5d4d3
Matt McPartlan <mmcpartlan98@gmail.com>
2018-11-10 02:24:58 -0800

    Generating tree works
    Next to add: creatign character codes

--- Homework/Homework03/Huffman.java
+++ Homework/Homework03/Huffman.java
@@ -12,17 +12,23 @@ import java.util.ArrayList;
 import java.util.*;
 
 class Huffman {
-  List<HuffmanNode> characters = Collections.synchronizedList(new ArrayList<HuffmanNode>(0));
+  public List<HuffmanNode> characters = Collections.synchronizedList(new ArrayList<HuffmanNode>(0));
+  public ListIterator<HuffmanNode> iterator;
+  public HuffmanNode root = new HuffmanNode('~');
+
   public void encode(String inputStr) {
     char[] input = inputStr.toCharArray();
     for (int i = 0; i < input.length; i++) {
-      characters.add(new HuffmanNode(input[i]));
+      HuffmanNode toAdd = new HuffmanNode(input[i]);
+      toAdd.isLeaf = true;
+      toAdd.count = 1;
+      characters.add(toAdd);
     }
-    ListIterator<HuffmanNode> iterator = characters.listIterator();
+    iterator = characters.listIterator();
+
     while (iterator.hasPrevious()) {
       iterator.previous();
     }
-
     while (iterator.hasNext()) {
       System.out.print(iterator.next().print() + ", ");
     }
@@ -49,7 +55,6 @@ class Huffman {
     while (iterator.hasPrevious()) {
       iterator.previous();
     }
-
     boolean sorted = false;
     while (!sorted) {
       sorted = true;
@@ -57,7 +62,7 @@ class Huffman {
         HuffmanNode tempFirst = iterator.next();
         if (iterator.hasNext()) {
           HuffmanNode tempSecond = iterator.next();
-          if (tempFirst.count < tempSecond.count) {
+          if (tempFirst.count > tempSecond.count) {
             // Relys on Java passing by reference (hopefully)
             char tfr = tempFirst.value;
             int tfrI = tempFirst.count;
@@ -70,29 +75,104 @@ class Huffman {
           iterator.previous();
         }
       }
-      while (iterator.hasPrevious()) {
-        iterator.previous();
-      }
     }
 
     while (iterator.hasPrevious()) {
       iterator.previous();
     }
+    System.out.print("Sorted: ");
     while (iterator.hasNext()) {
       System.out.print(iterator.next().print() + ", ");
     }
     System.out.println();
+
+    while (iterator.hasPrevious()) {
+      iterator.previous();
+    }
+
+    // Grow the huffman tree
+
+    while (characters.size() > 2) {
+      HuffmanNode babyTree = new HuffmanNode('ñ');
+      babyTree.children[0] = iterator.next();
+      if (iterator.hasNext()) {
+        iterator.remove();
+        babyTree.children[1] = iterator.next();
+        iterator.remove();
+        babyTree.count = babyTree.children[0].count + babyTree.children[1].count;
+        iterator.add(babyTree);
+      } else {
+        break;
+      }
+
+      // Sort again
+      while (iterator.hasPrevious()) {
+        iterator.previous();
+      }
+      boolean sortedSecond = false;
+      while (!sortedSecond) {
+        sortedSecond = true;
+        while (iterator.hasNext()) {
+          HuffmanNode tempFirst = iterator.next();
+          if (iterator.hasNext()) {
+            HuffmanNode tempSecond = iterator.next();
+            if (tempFirst.count > tempSecond.count) {
+              // Relys on Java passing by reference (hopefully)
+              char tfr = tempFirst.value;
+              int tfrI = tempFirst.count;
+              tempFirst.value = tempSecond.value;
+              tempFirst.count = tempSecond.count;
+              tempSecond.value = tfr;
+              tempSecond.count = tfrI;
+              sortedSecond = false;
+            }
+            iterator.previous();
+          }
+        }
+      }
+
+      while (iterator.hasPrevious()) {
+        iterator.previous();
+      }
+      while (iterator.hasNext()) {
+        System.out.print(iterator.next().print() + ", ");
+      }
+      System.out.println();
+
+      while (iterator.hasPrevious()) {
+        iterator.previous();
+      }
+
+    }
+    while (iterator.hasPrevious()) {
+      iterator.previous();
+    }
+
+    root.children[0] = iterator.next();
+    root.children[1] = iterator.next();
+    root.count = root.children[0].count + root.children[1].count;
+    System.out.println(root.print());
+
+    byte[] binaryNumber = new byte[root.count];
+    
+    HuffmanNode traverser = root.children[0];
+    for (int i = 0; i < root.children[0].count; i++) {
+
+    }
+
   }
 
-  public String decode(byte[] encoding) {
+  public String decode() {
+    System.out.println(iterator.next().value);
+    iterator.previous();
     return "no";
   }
 
   class HuffmanNode {
     public char value;
-    public int count = 1;
+    public int count = 0;
     public HuffmanNode[] children;
-    public HuffmanNode parent;
+    public boolean isLeaf = false;
 
     public HuffmanNode(char newChar) {
       this.value = newChar;
@@ -114,5 +194,7 @@ class Huffman {
     String testCase = "Hello world.";
     Huffman huff = new Huffman();
     huff.encode(testCase);
+    System.out.println("DECODE ===================================");
+    //huff.decode();
   }
 }
