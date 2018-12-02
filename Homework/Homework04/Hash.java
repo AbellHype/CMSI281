b220513ea94e25d2dad56e3fabac5d5bb34b1e31
Matt McPartlan <mmcpartlan98@gmail.com>
2018-12-01 16:39:14 -0800

    Hashign works

--- Homework/Homework04/Hash.java
+++ Homework/Homework04/Hash.java
@@ -19,29 +19,56 @@ class Hash {
     this.hashTable = new String[size];
     this.tableSize = size;
     for (int i = 0; i < tableSize; i++) {
-      tableSize[i] = "";
+      hashTable[i] = "";
     }
   }
 
   public boolean insert(String dataIn) {
     int idealIndex = getHash(dataIn);
     int index = idealIndex;
+    System.out.println(index);
     while (hashTable[index] != "") {
       index += 1;
-      if (index > hashTable.length() - 1) {
+      if (index > hashTable.length - 1) {
         index = 0;
       } else if (index == idealIndex) {
         return false;
       }
     }
-    hashTable[idealIndex] = dataIn;
+    hashTable[index] = dataIn;
     return true;
   }
 
-  private int getHash(String new) {
+  public String delete(String dataIn) {
+    int idealIndex = getHash(dataIn);
+    System.out.println("Index: " + idealIndex);
+    int index = idealIndex;
+    while (hashTable[index] != dataIn) {
+      index += 1;
+      if (index > hashTable.length - 1) {
+        index = 0;
+      } else if (index == idealIndex) {
+        return "";
+      }
+    }
+    String data = hashTable[index];
+    hashTable[index] = "";
+    return data;
+  }
+
+  public void display() {
+    System.out.print("Hash table: ");
+    for (int i = 0; i < hashTable.length; i++) {
+      System.out.print(hashTable[i] + ", ");
+    }
+    System.out.println();
+  }
+
+  private int getHash(String newData) {
     int sum = 0;
-    for (int i = 0; i < new.length(); i++) {
-      sum = (sum * 27 + new.charAt(i) - 96) % this.tableSize;
+    for (int i = 0; i < newData.length(); i++) {
+      sum = (sum * 27 + (newData.charAt(i) - 96)) % this.tableSize;
+      System.out.println("Sum: " + sum);
     }
     return sum;
   }
@@ -54,10 +81,31 @@ class Hash {
   * @return void
   */
   public static void main(String[] args) {
-    String testCase = "Hello world.";
-    Huffman huff = new Huffman();
-    huff.encode(testCase);
-    System.out.println("DECODE ===================================");
-    //huff.decode();
+    Hash hash = new Hash(10);
+    hash.insert("hello");
+    hash.insert("yoyo");
+    hash.insert("this bette go somewhere different");
+    hash.insert("one");
+    hash.insert("two");
+    hash.insert("three");
+    hash.insert("four");
+    hash.insert("five");
+    hash.insert("six");
+    hash.insert("seven");
+    hash.insert("nine");
+    hash.display();
+    hash.delete("yoyo");
+    hash.delete("hello");
+    hash.delete("yoyo");
+    hash.delete("this bette go somewhere different");
+    hash.delete("one");
+    hash.delete("two");
+    hash.delete("three");
+    hash.delete("four");
+    hash.delete("five");
+    hash.delete("six");
+    hash.delete("seven");
+    hash.delete("nine");
+    hash.display();
   }
 }
